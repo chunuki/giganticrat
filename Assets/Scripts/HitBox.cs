694488847041e014bea9add342a7f4f0fd2458ca
peopim <xaviercheahyk@gmail.com>
2026-02-04 13:27:39 +0800

    update hitbox

--- Assets/Scripts/HitBox.cs
+++ Assets/Scripts/HitBox.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
+    public Health health;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
