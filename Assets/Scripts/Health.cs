fbfc534a7bb9ab8885b5a6342982b101817dc870
peopim <xaviercheahyk@gmail.com>
2026-02-04 13:25:06 +0800

    stop pushing meeee

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
     public float currentHealth;
 
     Ragdoll ragdoll;
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
