fe63a0a8c266f3f60e3779cd1761844f2e30c2de
chunuki <darrenchaiyx@gmail.com>
2026-02-05 21:33:01 +0800

    toggle physics

--- Assets/Scripts/Playable Character/ThirdPersonShooterController.cs
+++ Assets/Scripts/Playable Character/ThirdPersonShooterController.cs
@@ -51,7 +51,7 @@ public class ThirdPersonShooterController : MonoBehaviour
             ResetMovementSpeed();
         }
 
-        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector3 mouseWorldPosition = Vector3.zero; 
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
