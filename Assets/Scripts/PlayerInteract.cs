3433758dbfc8b45bc475773d528cbf3fb3976eb5
chunuki <darrenchaiyx@gmail.com>
2026-02-05 16:52:26 +0800

    weapons

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -13,20 +13,20 @@ public class PlayerInteract : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (starterAssetsInputs.scan)
-        {
-            Debug.Log("Scan key was detected!");
-            float interactRange = 2.0f;
-            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
-            Debug.Log("Found " + colliderArray.Length + " colliders nearby.");
-            foreach (Collider collider in colliderArray)
-            {
-                if (collider.TryGetComponent<NPCInteractable>(out NPCInteractable npcInteractable))
-                {
-                    npcInteractable.Interact();
-                }
-            }
-            starterAssetsInputs.scan = false;
-        }
+        //if (starterAssetsInputs.scan)
+        //{
+        //    Debug.Log("Scan key was detected!");
+        //    float interactRange = 2.0f;
+        //    Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+        //    Debug.Log("Found " + colliderArray.Length + " colliders nearby.");
+        //    foreach (Collider collider in colliderArray)
+        //    {
+        //        if (collider.TryGetComponent<NPCInteractable>(out NPCInteractable npcInteractable))
+        //        {
+        //            npcInteractable.Interact();
+        //        }
+        //    }
+        //    starterAssetsInputs.scan = false;
+        //}
     }
 }
